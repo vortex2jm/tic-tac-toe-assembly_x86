fb6fd0403fb500667f0261db979d062cb05574da
vortex2jm <joaopaulomoura5328@gmail.com>
2023-10-03 00:42:05 -0300

    :sparkles: Creating entrypoint

--- main.asm
+++ main.asm
@@ -1,5 +1,37 @@
-org 0x100
+; DS
+segment data
 
-section .data
+; SS
+segment stack stack						
+		resb 256							; 256 bytes for stack
+stacktop:
 
-section .text
+; CS
+segment code
+..start:
+    ;Setting up segment registers
+    MOV 	AX,data						
+	MOV 	DS,AX
+	MOV 	AX,stack	
+	MOV 	ss,AX
+	MOV 	sp,stacktop
+
+    ;==============================;
+    entrypoint:
+        mov ah, 0x7
+        int 0x21
+        mov ah, 0x63    ;c character    
+        cmp ah, al
+        jz start_game  
+        mov ah, 0x73    ;s character
+        cmp ah, al
+        jz  end_game
+        jmp entrypoint
+
+    start_game:
+        mov ah, 0x4c
+        int 0x21
+
+    end_game:
+        mov ah, 0x4c
+        int 0x21
