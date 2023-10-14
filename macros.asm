76691b62fba9931f0daa846d99b8041367feb398
KPMGE <jesuskevin254@gmail.com>
2023-10-13 22:11:32 -0300

    :sparkles: add macros for checking if a player won

--- macros.asm
+++ macros.asm
@@ -2,6 +2,29 @@
 %ifndef macros_
 %define macros_
 
+%include "consts.asm"
+
+; verify_player_won(register_to_compare, label_to_jump)
+%macro check_player_won 2
+  compare_condition %1, PLAYER_WON_012, %2
+  compare_condition %1, PLAYER_WON_345, %2
+  compare_condition %1, PLAYER_WON_678, %2
+  compare_condition %1, PLAYER_WON_048, %2
+  compare_condition %1, PLAYER_WON_642, %2
+  compare_condition %1, PLAYER_WON_630, %2
+  compare_condition %1, PLAYER_WON_741, %2
+  compare_condition %1, PLAYER_WON_852, %2
+%endmacro
+
+; compare_condition(register_to_compare, variable_to_compare, label_to_jump)
+%macro compare_condition 3
+  push %1
+  and %1, %2
+  cmp %1, %2
+  pop %1
+  je %3
+%endmacro
+
 ; save_player_move(player_variable)
 %macro save_move 1
   mov bx, [%1]
